817d34ce7f7c853df2041ed2a957b1206da23e59
Elisee <mugiranezaelisee0@gmail.com>
2025-10-21 07:58:35 -0700

    Fixed hours

--- ESSA_ALARM.ino
+++ ESSA_ALARM.ino
@@ -15,38 +15,58 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", 2 * 3600, 60000);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void ringAlarm(int durationSec) {
+  lcd.print("It's Time");
   digitalWrite(ALARM_PIN, HIGH);
   delay(durationSec * 1000);
   digitalWrite(ALARM_PIN, LOW);
+  lcd.clear();
 }
 
 bool shouldRing(int dayOfWeek, int hour, int minute, int second) {
   if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-    if ((hour == 6 && minute == 0 && second == 0) ||
-        (hour == 7 && minute == 0 && second == 0)) {
+    if ((hour == 8 && minute == 40 && second == 0) ||
+        (hour == 9 && minute == 20 && second == 0) ||
+        (hour == 10 && minute == 0 && second == 0) ||
+        (hour == 11 && minute == 50 && second == 0) ||
+        (hour == 12 && minute == 30 && second == 0) ||
+        (hour == 14 && minute == 50 && second == 0) ||
+        (hour == 15 && minute == 30 && second == 0)) {
       ringAlarm(6);
       return true;
     }
-    if ((hour == 10 && minute == 0 && second == 0) ||
-        (hour == 12 && minute == 0 && second == 0)) {
+    if ((hour == 8 && minute == 0 && second == 0) ||
+        (hour == 10 && minute == 40 && second == 0) ||
+        (hour == 11 && minute == 10 && second == 0) ||
+        (hour == 13 && minute == 10 && second == 0) ||
+        (hour == 14 && minute == 10 && second == 0) ||
+        (hour == 16 && minute == 10 && second == 0) ||
+        (hour == 18 && minute == 0 && second == 0) ||
+        (hour == 19 && minute == 0 && second == 0) ||
+        (hour == 21 && minute == 0 && second == 0)) {
       ringAlarm(10);
       return true;
     }
-    if ((dayOfWeek == 1 || dayOfWeek == 4 || dayOfWeek == 5) &&
+    if ((dayOfWeek == 1 || dayOfWeek == 3 || dayOfWeek == 5) &&
         hour == 7 && minute == 30 && second == 0) {
-      ringAlarm(6);
+      ringAlarm(15);
       return true;
     }
   }
-  if (dayOfWeek == 6 || dayOfWeek == 0) {
-    if ((hour == 8 && minute == 0 && second == 0) ||
-        (hour == 12 && minute == 0 && second == 0) ||
-        (hour == 18 && minute == 0 && second == 0)) {
-      ringAlarm(6);
+  if (dayOfWeek == 0) {
+    if ((hour == 7 && minute == 10 && second == 0) ||
+        (hour == 12 && minute == 45 && second == 0) ||
+        (hour == 18 && minute == 0 && second == 0) ||
+        (hour == 19 && minute == 0 && second == 0) ||
+        (hour == 21 && minute == 0 && second == 0)) {
+      ringAlarm(15);
       return true;
     }
-    if ((hour == 20 && minute == 0 && second == 0)) {
-      ringAlarm(10);
+  }
+  if (dayOfWeek == 6) {
+    if ((hour == 7 && minute == 10 && second == 0) ||
+        (hour == 12 && minute == 45 && second == 0) ||
+        (hour == 18 && minute == 0 && second == 0)) {
+      ringAlarm(15);
       return true;
     }
   }
@@ -56,11 +76,33 @@ bool shouldRing(int dayOfWeek, int hour, int minute, int second) {
 void setup() {
   pinMode(ALARM_PIN, OUTPUT);
   digitalWrite(ALARM_PIN, LOW);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
-  timeClient.begin();
   lcd.init();
   lcd.backlight();
+
+  WiFi.begin(ssid, password);
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    lcd.print(".");
+    delay(500);
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("WiFi Connected");
+  delay(2000);
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("made by ELONDA");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("ESSA ALARM");
+  delay(3000);
+  lcd.clear();
+
+
+  timeClient.begin();
 }
 
 void loop() {
@@ -71,20 +113,18 @@ void loop() {
   int minute = timeClient.getMinutes();
   int second = timeClient.getSeconds();
 
+  const char* days[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
   lcd.setCursor(0, 0);
-  lcd.print("Day:");
-  lcd.print(dayOfWeek);
+  lcd.print("Day: ");
+  lcd.print(days[dayOfWeek-1]);
+  int rem = 16 - (5 + strlen(days[dayOfWeek-1]));
+  for(int i=0;i<rem;i++) lcd.print(" ");
+
   lcd.setCursor(0, 1);
-  if (hour < 10) lcd.print("0");
-  lcd.print(hour);
-  lcd.print(":");
-  if (minute < 10) lcd.print("0");
-  lcd.print(minute);
-  lcd.print(":");
-  if (second < 10) lcd.print("0");
-  lcd.print(second);
+  char timeStr[17];
+  snprintf(timeStr,sizeof(timeStr),"Time : %02d:%02d:%02d",hour,minute,second);
+  lcd.print(timeStr);
 
-  shouldRing(dayOfWeek, hour, minute, second);
-
+  shouldRing(dayOfWeek,hour,minute,second);
   delay(1000);
 }
